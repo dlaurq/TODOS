dc7cde698a8bcc76a7c419029f452794008d7034
Laurentiu Dutu <qlaurd@gmail.com>
2023-10-03 12:21:03 +0300

    Am adaugat functionalitatea de a adauga un task in DB

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import TodosInput from './components/TodosInput';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Todos</h1>
+    <main className="min-w-max min-h-screen flex flex-col items-center gap-5">
+      <h1 className="text-4xl font-bold text-red-400 pt-10">Todos</h1>
+      <TodosInput />
     </main>
   );
 }
