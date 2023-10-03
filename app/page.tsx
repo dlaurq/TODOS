0bd7e4542eeb2b30ac16caf8f89e067415dcd146
Laurentiu Dutu <qlaurd@gmail.com>
2023-10-03 12:35:43 +0300

    Am adaugat vizualizarea task-urilor

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import TodosInput from './components/TodosInput';
+import prisma from './lib/prisma';
 
-export default function Home() {
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
+
   return (
-    <main className="min-w-max min-h-screen flex flex-col items-center gap-5">
-      <h1 className="text-4xl font-bold text-red-400 pt-10">Todos</h1>
-      <TodosInput />
+    <main className="min-w-max min-h-screen flex justify-center items-start gap-5 bg-gray-200">
+      <section className="flex flex-col justify-between h-full">
+        <h1 className="text-4xl font-bold text-red-400 pt-10 pb-5 text-center">
+          Todos
+        </h1>
+        <TodosInput />
+        {todos.map((todo) => (
+          <section
+            key={todo.id}
+            className="bg-white border-b-2 border-b-gray-200 p-5"
+          >
+            <p>{todo.title}</p>
+          </section>
+        ))}
+      </section>
     </main>
   );
 }
